bdddc82bb22d645804b538706535d3f7de04b9e2
Zsolt Nagy <zsolt.nagy@vodafone.com>
2020-07-22 16:10:47 +0200

    added text to header

--- vodafone-global-storefront-theme/config/index.js
+++ vodafone-global-storefront-theme/config/index.js
@@ -3,8 +3,8 @@
 const config = {
 	env: "dev",
 	theme: "vodafone",
-	marketplacePath: "https://testvfgreece.appdirect.com",
-	pageObjectsPath: "https://testvfgreece.appdirect.com/api/internal/storefront/v1",
+	marketplacePath: "https://testvfuk.appdirect.com",
+	pageObjectsPath: "https://testvfuk.appdirect.com/api/internal/storefront/v1",
 	publicCSSGenPath: "/assets/css/pages",
 	publicJSPath: "/assets/css/images",
 	publicImagesPath: "/assets/js",
